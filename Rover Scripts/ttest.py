92e85ffdf0f9549a20815df29776ed54fdb5112f
James Talbert <jtalbert@iastate.edu>
2018-07-30 17:19:27 -0500

    Added interface scripts
    not yet documented

---
+++ Rover Scripts/ttest.py
@@ -0,0 +1,12 @@
+import phoenix
+import time
+
+rover = phoenix.Phoenix('192.168.1.11')
+#rover.start()
+
+for i in range(10):
+    rover.setWheels(20,20)
+    print(rover.temperature)
+    time.sleep(0.1)
+rover.setWheels(0,0)    
+#rover.close()
